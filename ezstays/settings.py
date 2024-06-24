411a0d3a187b430f8d7293d31ac0eb40644fe9c9
blackheart4dev <blackheart4dev@hotmail.com>
2024-06-24 19:43:43 +0530

    update gmail password

--- ezstays/settings.py
+++ ezstays/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "aezstays@gmail.com"
-EMAIL_HOST_PASSWORD = "nstn mltj jnhd lgbl"
+EMAIL_HOST_PASSWORD = "ladm sybz gkcl phid"
